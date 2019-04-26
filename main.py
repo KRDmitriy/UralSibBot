08680bbb754cf861e6f289e82ff39874c5ee0564
KRDmitriy <karadmitrii@gmail.com>
2019-04-26 23:12:20 +0300

    Hello, world!

---
+++ main.py
@@ -0,0 +1,15 @@
+import telebot
+
+bot = telebot.TeleBot("867167395:AAE8915Oh6QMTq3UGT_KxdKnoCV9d-1V1cU")
+
+@bot.message_handler(content_types=['text'])
+def get_text_messages(message):
+    if message.text == "Привет":
+        bot.send_message(message.from_user.id, "Привет, чем я могу тебе помочь?")
+    elif message.text == "/help":
+        bot.send_message(message.from_user.id, "Напиши привет")
+    else:
+        bot.send_message(message.from_user.id, "Я тебя не понимаю. Напиши /help.")
+
+
+bot.polling(none_stop=True, interval=0)
